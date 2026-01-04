aeae0989f4092e94549ae74c5811842e12849c55
Florian Giesemann <florian.giesemann@gmail.com>
2026-01-04 17:30:23 +0100

    Split GCC and Clang settings

--- CompilerSettings.cmake
+++ CompilerSettings.cmake
@@ -3,9 +3,12 @@ set(ARCH_LEVEL "v3" CACHE STRING "x86-64 Architecture Level: generic, v2, v3, v4
 
 function(add_optimization_settings TARGET_NAME)
     target_compile_options(${TARGET_NAME} PRIVATE
-        $<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-O3;-march=$<IF:$<STREQUAL:${ARCH_LEVEL},native>,native,x86-64-${ARCH_LEVEL}>
         $<$<AND:$<CXX_COMPILER_ID:GNU>,$<BOOL:${ENABLE_LTO}>>:-flto>
+        $<$<CXX_COMPILER_ID:GNU>:-O3;-march=$<IF:$<STREQUAL:${ARCH_LEVEL},native>,native,x86-64-${ARCH_LEVEL}>
+
         $<$<AND:$<CXX_COMPILER_ID:Clang>,$<BOOL:${ENABLE_LTO}>>:-flto=thin>
+        $<$<CXX_COMPILER_ID:Clang>:-O3;-march=$<IF:$<STREQUAL:${ARCH_LEVEL},native>,native,x86-64-${ARCH_LEVEL}>
+
         $<$<CXX_COMPILER_ID:MSVC>:/O2;/Oi;/Ot;/GT;$<$<STREQUAL:${ARCH_LEVEL},v3>:/arch:AVX2>;$<$<STREQUAL:${ARCH_LEVEL},v4>:/arch:AVX512>;$<$<BOOL:${ENABLE_LTO}>:/GL>;/W4;/permissive->
     )
 
